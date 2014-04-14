b051198d0dd5c0401f31dd44627f66b4edc0d310
bgernert <mail@bjoern-gernert.de>
2014-04-14 08:31:59 +0200

    Created Tag_String

---
+++ data/Tag_String.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Tag_String.php
+ *
+ * Copyright (C) 2014 Björn Gernert
+ *
+ * Written-by: Björn Gernert <mail@bjoern-gernert.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace bgernert\libNBT;
+
+class Tag_String implements Tag {
+ 
+    /**
+     * Stores the name of the tag
+     * 
+     * @var string
+     */
+    private $name = '';
+    
+    /**
+     * Stores the value of the tag
+     * 
+     * @var string
+     */
+    private $value = '';
+    
+    public function setName(&$name) {
+        $this->name = $name;
+    }
+    
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setValue(&$value) {
+        //TODO: Check for string
+        $this->value = $value;
+    }
+
+    public function getValue() {
+        return $this->value;
+    }
+    
+    public function getLength() {
+        return strlen($this->value);
+    }
+
+    public function __toString() {
+        return "TAG_String(\"{$this->name}\") {$this->value}\n";
+    }
+}
